d50b88e3118b9e310151741a85b3d3132b0d134b
Tony Kordysh <tkordysh@gmail.com>
2022-03-07 17:49:58 -0600

    passing all tests

--- src/index.js
+++ src/index.js
@@ -40,3 +40,96 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+//HEADER SECTION
+
+// NAV SECTION
+//links section
+const navLinks = document.querySelectorAll('header nav a');
+
+navLinks[0].textContent = siteContent.nav["nav-item-1"];
+navLinks[1].textContent = siteContent.nav["nav-item-2"];
+navLinks[2].textContent = siteContent.nav["nav-item-3"];
+navLinks[3].textContent = siteContent.nav["nav-item-4"];
+navLinks[4].textContent = siteContent.nav["nav-item-5"];
+navLinks[5].textContent = siteContent.nav["nav-item-6"];
+
+//adding italics 
+navLinks[0].classList.add('italic');
+navLinks[1].classList.add('italic');
+navLinks[2].classList.add('italic');
+navLinks[3].classList.add('italic');
+navLinks[4].classList.add('italic');
+navLinks[5].classList.add('italic');
+
+//adding img
+const logoImg = document.querySelector(".logo");
+logoImg.src = "http://localhost:9000/img/logo.png";
+
+
+//CTA SECTION
+//h1 element 
+const siteTitle = document.querySelector("h1");
+//h1 setting text content 
+siteTitle.textContent = siteContent["cta"]["h1"];
+//button element
+const buttonElem = document.querySelector("button");
+//button setting text content 
+buttonElem.textContent = siteContent["cta"]["button"];
+//cta img 
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.src = "http://localhost:9000/img/cta.png"
+
+
+//MAIN CONTENT SECTION
+//TOP and BOTTOM CONTENT VARIABLES
+const topContent = document.querySelector(".top-content");
+const bottomContent = document.querySelector(".bottom-content");
+
+//separating h tags and p tags 
+
+//top content h tags
+const topHTags = topContent.querySelectorAll(".text-content h4");
+topHTags[0].textContent = siteContent["main-content"]["features-h4"];
+topHTags[1].textContent = siteContent["main-content"]["about-h4"];
+
+//bottom content h tags
+const bottomHTags = bottomContent.querySelectorAll(".text-content h4");
+bottomHTags[0].textContent = siteContent["main-content"]["services-h4"];
+bottomHTags[1].textContent = siteContent["main-content"]["product-h4"];
+bottomHTags[2].textContent = siteContent["main-content"]["vision-h4"];
+
+//top content p tags
+const topPTags = topContent.querySelectorAll(".text-content p");
+topPTags[0].textContent = siteContent["main-content"]["features-content"];
+topPTags[1].textContent = siteContent["main-content"]["about-content"];
+
+//bottom content p tags
+const bottomPTags = bottomContent.querySelectorAll(".text-content p");
+bottomPTags[0].textContent = siteContent["main-content"]["services-content"];
+bottomPTags[1].textContent = siteContent["main-content"]["product-content"];
+bottomPTags[2].textContent = siteContent["main-content"]["vision-content"];
+
+//adding main cont img
+const mainContImg = document.querySelector(".middle-img")
+mainContImg.src = "http://localhost:9000/img/accent.png"
+
+
+//CONTACT SECTION
+//setting contact variable
+const contactSect = document.querySelector(".contact");
+//contact h4
+const contactH4 = contactSect.querySelector("h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+//contact p tags
+const contactPTags = contactSect.querySelectorAll("p");
+contactPTags[0].textContent = siteContent.contact.address;
+contactPTags[1].textContent = siteContent.contact.phone;
+contactPTags[2].textContent = siteContent.contact.email;
+
+
+//FOOTER SECTION
+const copyrightLink = document.querySelector("footer a");
+copyrightLink.textContent = siteContent.footer.copyright;
+
+copyrightLink.classList.add('bold');
